14894aa02ec2df30c58e32893ecc69be6990466c
GeorgiYolovski <bluef0rever@abv.bg>
2013-09-01 17:43:17 +0300

    Added conversation-view-model.js

---
+++ DoubleGChat/js/view-models/conversation-view-model.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+
+    var currentConversation = { };
+    
+
+    WinJS.Namespace.define("DoubleGChat.ViewModels.Conversation", {
+        currentConversation: WinJS.Binding.as(currentConversation),
+        partner: WinJS.Binding.as(currentConversation.)
+    });
+})();
