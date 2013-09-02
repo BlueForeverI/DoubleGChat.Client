5b68fc8be873e0ab92e3844ffe61fe186f09d97b
GeorgiYolovski <bluef0rever@abv.bg>
2013-09-02 11:13:21 +0300

    Settings charm is now working.

--- DoubleGChat/pages/profile-settings/profile-settings.js
+++ DoubleGChat/pages/profile-settings/profile-settings.js
@@ -9,6 +9,8 @@
         ready: function (element, options) {
             var firstName = document.getElementById("first-name");
             var lastName = document.getElementById("last-name");
+            var oldPassword = document.getElementById("old-password");
+            var newPassword = document.getElementById("new-password");
             var saveButton = document.getElementById("save-settings");
             
             var userInfo = DoubleGChat.Controllers.User.getUserCredentials();
@@ -16,7 +18,27 @@
             lastName.value = userInfo.lastName;
 
             saveButton.addEventListener("click", function() {
-                // TODO : send update information to service
+                var firstNameValue = firstName.value;
+                var lastNameValue = lastName.value;
+                var oldPasswordValue = oldPassword.value;
+                var newPaswordValue = newPassword.value;
+                var oldPasswordHash = (oldPasswordValue) ? CryptoJS.SHA1(oldPasswordValue).toString() : null;
+                var newPasswordHash = (newPaswordValue) ? CryptoJS.SHA1(newPaswordValue).toString() : null;
+
+                var editUserObject = {
+                    firstName: (firstNameValue) ? firstNameValue : null,
+                    lastName: (lastNameValue) ? lastNameValue : null,
+                    oldPasswordHash: oldPasswordHash,
+                    newPasswordHash: newPasswordHash
+                };
+
+                var editUrl = DoubleGChat.Constants.baseUrl + "users/edit";
+                DoubleGChat.RemoteData.sendRequest(editUrl, "post", editUserObject, userInfo.sessionKey)
+                    .done(function (success) {
+                        var x = 0;
+                    }, function (error) {
+                        var y = 0;
+                    });
             });
         },
 
