9cc4cf5a77baa634d4f582d67ac8ff6dcfa22a4f
Georgi <georgi.vorobiov@gmail.com>
2013-09-03 14:15:18 +0300

    Added appbar

--- DoubleGChat/js/default-code-behind.js
+++ DoubleGChat/js/default-code-behind.js
@@ -12,19 +12,29 @@
         WinJS.Navigation.navigate("/pages/register/register.html");
     };
 
+    var goToLogin = function (event) {
+        WinJS.Navigation.navigate("/pages/login/login.html");
+    };
+
     var navigateToContactRequests = function (event) {
         WinJS.Navigation.navigate("/pages/contact-requests/contact-requests.html");
     };
 
-
-    var navigateToConversations = function () {
-        WinJS.Navigation.navigate("/pages/missed-conversations/missed-conversations.html");
+    var navigateToContacts = function () {
+        WinJS.Navigation.navigate("/pages/contacts/contacts.html");
     };
+
+    WinJS.Utilities.markSupportedForProcessing(navigateToContacts);
+    WinJS.Utilities.markSupportedForProcessing(navigateToContactRequests);
+    WinJS.Utilities.markSupportedForProcessing(goToRegister);
+    WinJS.Utilities.markSupportedForProcessing(logoutHandler);
+    WinJS.Utilities.markSupportedForProcessing(goToLogin);
 
     WinJS.Namespace.define("DoubleGChat.CodeBehind.Default", {
         logout: logoutHandler,
         goToRegister: goToRegister,
         navigateToContactRequests: navigateToContactRequests,
-        navigateToConversations: navigateToConversations
+        navigateToContacts: navigateToContacts,
+        goToLogin: goToLogin
     });
 })();
